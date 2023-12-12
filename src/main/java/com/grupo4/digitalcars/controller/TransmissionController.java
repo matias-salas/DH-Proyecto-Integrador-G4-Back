c6e87b0b33456c14ef3dc4fb9441e759e2a61ded
matias-salas <matiasalas.e@gmail.com>
2023-12-12 15:29:49 -0300

    transmission in tag

--- src/main/java/com/grupo4/digitalcars/controller/TransmissionController.java
+++ src/main/java/com/grupo4/digitalcars/controller/TransmissionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@Api(tags="Cities")
+@Api(tags="Transmission")
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/transmission")
